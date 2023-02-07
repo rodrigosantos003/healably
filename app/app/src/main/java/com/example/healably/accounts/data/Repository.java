1c8aba3f89998694b87f1d99b9a84476ff11265d
Rodrigo Santos <rodrigo.s.santos003@gmail.com>
2023-02-07 12:43:51 +0000

    Increased edit text height

---
+++ app/app/src/main/java/com/example/healably/accounts/data/Repository.java
@@ -0,0 +1,9 @@
+package com.example.healably.accounts.data;
+
+import com.example.healably.accounts.model.User;
+
+import java.util.ArrayList;
+
+public class Repository {
+    public ArrayList<User> users;
+}
